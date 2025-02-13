73ca99cba94f33e206cd21ae3131ed2792f5ac3f
Lucas-Mafra <lucas.lima.nave@gmail.com>
2025-02-13 20:03:30 -0300

    first commit

---
+++ src/main/java/lucidez/apispringboot/ApispringbootApplication.java
@@ -0,0 +1,13 @@
+package lucidez.apispringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApispringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApispringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lucidez/apispringboot/ApispringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package lucidez.apispringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApispringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
